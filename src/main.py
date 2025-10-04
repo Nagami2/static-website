a3cfaf3c321c7efbac9fdf13644be01b9af7f325
Nagami2 <nagababukatakam2@gmail.com>
2025-10-04 16:31:14 -0400

    add textnode enum

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    node1 = TextNode("This is some anchor text", TextType.LINK.value, url="https://example.com")
+    print(node1)
+
+if __name__ == "__main__":
+    main()
